aac6a949a4fd8f12f602dcfb90bf538e1afcd02c
dajeong01 <wpekwjd@naver.com>
2025-04-24 17:47:31 +0900

    "feat : 20250424"

--- src/main/java/ch09_classes/Car.java
+++ src/main/java/ch09_classes/Car.java
@@ -4,7 +4,8 @@ public class Car {
     String color;
     int speed;
     String speedGrade;
-//    String eval = (speed > 100 ? "빠릅니다." : "그냥저냥입니다.");
+// String eval = (speed > 100 ? "빠릅니다." : "그냥저냥입니다.");
+// 이거는 speed를 초기값을 0으로 지정했기에 코드를 여기다 넣을시 무슨 차든 속도가 그냥저냥이 나옴
 
 
     public void drive(){
@@ -18,7 +19,17 @@ public class Car {
 
     public void displayCarInfo(){
         String eval = (speed > 100 ? "빠릅니다." : "그냥저냥입니다.");
+        // 여기서 설정할경우 실시간으로 그때의 speed 값을 보고 판단하는 코드이기에
+        // 객체의 speed가 200일시 빠릅니다가 나옴
+
         System.out.println("이 차의 색깔은 " + color + "이고," +
                 " \n최고 속도는" +speed + " km/h입니다. " + eval);
     }
 }
+/*
+        7번줄과 20번줄 String eval = (speed > 100 ? "빠릅니다." : "그냥저냥입니다."); 위치가 중요한 이유
+        : 클래스 변수는 객체가 만들어질 때 자동 실행
+          메서드는 내가 호출해야 실행되는 행동
+
+
+ */
--- src/main/java/ch09_classes/CarMain.java
+++ src/main/java/ch09_classes/CarMain.java
@@ -4,7 +4,8 @@ public class CarMain {
     public static void main(String[] args) {
 
         Car car1 = new Car();
-        Car car2 = new Car();
+        Car car2 = new Car();// 객체 생성시에 기본생성자이기 때문에
+        // color = null, speed = 0, speedGrade = null, eval = "그냥저냥입니다"로 생성
 
         car1.color = "빨강";
         car1.speed = 100;
