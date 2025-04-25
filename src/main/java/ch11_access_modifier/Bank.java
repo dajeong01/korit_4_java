49e4590df9c9a919d23215c595ddee304835ffbc
dajeong01 <wpekwjd@naver.com>
2025-04-25 17:40:27 +0900

    "feat : 20250425 Setter / Getter"

--- src/main/java/ch11_access_modifier/Bank.java
+++ src/main/java/ch11_access_modifier/Bank.java
@@ -1,7 +1,5 @@
 package ch11_access_modifier;
 
-import java.util.Scanner;
-
 public class Bank {
 
     // 생성자
@@ -14,7 +12,7 @@ public class Bank {
     Bank(int accountNum){}
     Bank(int accountNum, String accountHolder){}
     Bank(int accountNum, String accountHolder, int balance){}
-    Bank(int accountNum, String accountHolder, int balance,int pinNumber){}
+    Bank(int accountNum, String accountHolder, int balance, int pinNumber){}
 
     // 메서드
 
@@ -113,4 +111,73 @@ public class Bank {
         System.out.println();
     }
 }
+
+//// 여기서부터는 선생님 풀이
+//public void setBalance(int balance) {
+//    // 0원 이하는 입력 불가능
+//    if (balance < 1) {
+//        System.out.println("불가능한 입력 금액입니다.");
+//        return;
+//    }
+//    this.balance = balance;
+//}
+//
+//public int getPinNumber() {
+//    return pinNumber;
+//}
+//
+//// 얘도요.
+//public void setPinNumber(int pinNumber) {
+//    if(pinNumber < 0 || pinNumber > 9999) {
+//        System.out.println("불가능한 비밀번호입니다");
+//        return;
+//    }
+//    this.pinNumber = pinNumber;
+//}
+//
+//
+//// 나머지 메서드들
+//
+//public void deposit(int amount, int inputPin) {
+//    if (this.pinNumber != inputPin) {
+//        // 종료
+//        System.out.println("비밀번호가 틀렸습니다.");
+//        return;
+//    }
+//    // 비밀번호와 입력 비밀번호가 동일하다면 진행
+//    if (amount < 0) {
+//        System.out.println("불가능한 입금 금액입니다.");
+//        return;
+//    }
+//    balance += amount;
+////        System.out.println("입금 성공! 현재 잔액 : " + this.balance); // 오류 안남
+//    System.out.println("입금 성공! 현재 잔액 : " + balance);    // 오류 안남
+//}
+//
+//public void withdraw(int amount, int inputPin) {
+//    if (this.pinNumber != inputPin) {
+//        // 종료
+//        System.out.println("비밀번호가 틀렸습니다.");
+//        return;
+//    }
+//    if (amount < 1) {
+//        System.out.println("불가능한 출금 금액입니다.");
+//        return;
+//    }
+//    if (balance - amount < 0) {
+//        System.out.println("잔액이 부족하여 출금할 수 없습니다.");
+//        return;
+//    }
+//    balance -= amount;
+//    System.out.println(amount + "원이 출금되었습니다. 현재 잔액 : " + balance);
+//}
+//
+//public void showAccountInfo() {}
+//
+//public void displayInfo() {
+//    System.out.println("\n계좌 소유자 : " + accountHolder);
+//    System.out.println("계좌 번호 : " + accountNum);
+//    System.out.println("현재 잔액 : " + balance + "\n");
+//}
+//}
 
--- src/main/java/ch11_access_modifier/BankMain.java
+++ src/main/java/ch11_access_modifier/BankMain.java
@@ -2,34 +2,34 @@ package ch11_access_modifier;
 
 public class BankMain {
     public static void main(String[] args) {
-        Bank bank1 = new Bank();
-        Bank bank2 = new Bank();
+        Bank bankAlone1 = new Bank();
+        Bank bankAlone2 = new Bank();
 
-        bank1.setAccountHolder("홍길동");
-        bank1.setBalance(100000);
-        bank1.setPinNumber(1111);
-        bank1.setAccountNum(123456);
+        bankAlone1.setAccountHolder("홍길동");
+        bankAlone1.setBalance(100000);
+        bankAlone1.setPinNumber(1111);
+        bankAlone1.setAccountNum(123456);
 
-        bank2.setAccountHolder("신사임당");
-        bank2.setBalance(500000);
-        bank2.setPinNumber(2222);
-        bank2.setAccountNum(987654);
+        bankAlone2.setAccountHolder("신사임당");
+        bankAlone2.setBalance(500000);
+        bankAlone2.setPinNumber(2222);
+        bankAlone2.setAccountNum(987654);
 
-        bank1.displayinfo();
-        bank2.displayinfo();
+        bankAlone1.displayinfo();
+        bankAlone2.displayinfo();
 
-        bank1.deposit(50000, 1111);
-        bank1.withdraw(200000, 1111);
-        bank1.withdraw(100000, 1111);
-
-        System.out.println();
-
-        bank2.withdraw(100000,2222);
-        bank2.deposit(200000,2222);
+        bankAlone1.deposit(50000, 1111);
+        bankAlone1.withdraw(200000, 1111);
+        bankAlone1.withdraw(100000, 1111);
 
         System.out.println();
 
-        bank1.displayinfo();
-        bank2.displayinfo();
+        bankAlone2.withdraw(100000,2222);
+        bankAlone2.deposit(200000,2222);
+
+        System.out.println();
+
+        bankAlone1.displayinfo();
+        bankAlone2.displayinfo();
     }
 }
