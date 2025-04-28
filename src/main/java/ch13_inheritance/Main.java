0688750e69129f0fa945af2001ffd79272a47c3d
dajeong01 <wpekwjd@naver.com>
2025-04-28 17:41:22 +0900

    feat : 20250428 /array/inheritance/method overriding

--- src/main/java/ch13_inheritance/Main.java
+++ src/main/java/ch13_inheritance/Main.java
@@ -1,42 +1,36 @@
 package ch13_inheritance;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Animal animal1 = new Animal();
         animal1.setAnimalName("나비");
         animal1.setAnimalAge(1);
         animal1.move();
-
-        System.out.println();
 
         Tiger tiger1 = new Tiger();
         tiger1.setAnimalName("푸바오");
         tiger1.setAnimalAge(3);
         tiger1.move();
         tiger1.hunt();
-//        animal1.hunt;     // 오류 발생 -> 부모클래스는 자식클래스의 필드 및 메서드에 접근 불가능
-        tiger1.setStrpied(true);
-//        animal1.setStriped(false) // 오류 발생 -> 18번 라인과 동일한 이유로
+//        animal1.hunt();   오류 발생 -> 부모클래스는 자식클래스의 필드 및 메서드에 접근 불가능
+        tiger1.setStriped(true);
+//        animal1.setStripted(false); 오류 발생 -> 16번 라인과 동일한 이유로
 
-        System.out.println();
 
         Human human1 = new Human();
+        human1.setAnimalName("여러분이름");
+        human1.setAnimalAge(19);
+        System.out.println(human1.getAnimalName());
+
+
+        System.out.println("재정의한 getAnimalAge()를 호출합니다.");
+        int age = human1.getAnimalAge();
+        System.out.println("나이를 출력합니다 : " + age);
+
+
         human1.move();
-        human1.setAnimalName("제다정");
-        human1.setAnimalAge(25);
-        human1.read("자바의기초");
-
-
-
-
-
-
-
-
-
-
-
+        human1.read("자바의 기초");
 
     }
 }
